a3a0805eb4a120ede3317d78aea35919d82c46ac
Bryazgin Sergey <sergeybryazgin@gmail.com>
2024-01-11 23:14:37 +0300

    added api tests

--- tests/test_restful_booker.py
+++ tests/test_restful_booker.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 import jsonschema
 from qa_guru_diploma_api.utils.load_schema import load_schema
@@ -7,18 +8,23 @@ from qa_guru_diploma_api.utils.help_functions import get_token, get_id, get_id_t
 def test_get_booking(default_url):
     id = get_id()
     headers = {'Accept': 'application/json'}
-    result = requests.get(f'{default_url}booking/{id}',
+    with allure.step('Отправить GET запрос на эндпоинт "/booking/{id}"'):
+        result = requests.get(f'{default_url}booking/{id}',
                           headers=headers)
     schema = load_schema('get_booking.json')
 
-    assert result.status_code == 200
-    jsonschema.validate(result.json(), schema)
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить json схему ответа'):
+        jsonschema.validate(result.json(), schema)
 
 
 def test_ping_healthcheck_status_code(default_url):
-    result = requests.get(f'{default_url}ping')
+    with allure.step('Отправить GET запрос на эндпоинт "/ping"'):
+        result = requests.get(f'{default_url}ping')
 
-    assert result.status_code == 201
+    with allure.step('Проверить, что статус код равен 201'):
+        assert result.status_code == 201
 
 
 def test_create_booking(default_url):
@@ -34,11 +40,14 @@ def test_create_booking(default_url):
         },
         "additionalneeds": "Breakfast"
     }
-    result = requests.post(f'{default_url}booking', json=booking, headers=headers)
+    with allure.step('Отправить POST запрос на эндпоинт "/booking"'):
+        result = requests.post(f'{default_url}booking', json=booking, headers=headers)
     schema = load_schema('create_booking.json')
 
-    assert result.status_code == 200
-    jsonschema.validate(result.json(), schema)
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить json схему ответа'):
+        jsonschema.validate(result.json(), schema)
 
 
 def test_delete_booking(default_url):
@@ -47,9 +56,11 @@ def test_delete_booking(default_url):
     headers = {'Content-Type': 'application/json',
                'Cookie': token}
     id = get_id_to_delete()
-    result = requests.delete(f'{default_url}booking/{id}', headers=headers)
+    with allure.step('Отправить DELETE запрос на эндпоинт "/booking/{id}"'):
+        result = requests.delete(f'{default_url}booking/{id}', headers=headers)
 
-    assert result.status_code == 201
+    with allure.step('Проверить, что статус код равен 201'):
+        assert result.status_code == 201
 
 
 def test_update_booking(default_url):
@@ -70,7 +81,10 @@ def test_update_booking(default_url):
         },
         "additionalneeds": "Breakfast"
     }
-    result = requests.put(f'{default_url}booking/{id}', json=booking, headers=headers)
+    with allure.step('Отправить PUT запрос на эндпоинт "/booking/{id}"'):
+        result = requests.put(f'{default_url}booking/{id}', json=booking, headers=headers)
 
-    assert result.status_code == 200
-    assert result.json()['lastname'] == 'White'
+    with allure.step('Проверить, что статус код равен 200'):
+        assert result.status_code == 200
+    with allure.step('Проверить, что значение параметра lastname в ответе равно "White"'):
+        assert result.json()['lastname'] == 'White'
